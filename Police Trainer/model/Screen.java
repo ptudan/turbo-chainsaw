1410d3e6e23065c2daabc283e1e13742162a1040
Paul Tudan <ptudan@udel.edu>
2017-07-06 13:28:46 -0400

    window spawner created

--- Police Trainer/model/Screen.java
+++ Police Trainer/model/Screen.java
@@ -1,13 +1,44 @@
 package view;
 import java.util.*;
+import java.awt.Rectangle;
 
 public class Screen{
 	
 	private final static int boardHeight = 600;
 	private final static int boardWidth = 800;
 	private int minWindowWidth = 50;
+	private int maxWindowWidth = 150;
 	private int minTime = 500;
 	private List<Window> windows = new ArrayList<Window>();
+	private int score = 0;
+	private Random r = new Random();
 
+	public Screen(){
+
+	}
+
+	private void spawnWindows(int count){
+		if(count < 0 || count > 8) return;
+
+		while(windows.size()<count){
+			boolean repeat = true;
+			int width, height, x, y;
+			while(repeat){
+				repeat = false;
+				width = minWindowWidth + r.nextInt(maxWindowWidth - minWindowWidth);
+				height = width*1.6;
+				x = r.nextInt(boardWidth-width);
+				y = r.nextInt(boardHeight-height);
+				Rectangle r = new Rectangle(x, y, width, height);
+				for(Window w : windows){
+					if(w.clashesWith(r)){
+						repeat = true;
+						continue;
+					}
+				}
+			}
+			window.add(new Window(x, y, width, height, 2500));
+		}
+	}
 
 }
--- Police Trainer/model/Window.java
+++ Police Trainer/model/Window.java
@@ -1,4 +1,4 @@
-
+import java.awt.Rectangle;
 
 public class Window{
 	private int x;
@@ -8,12 +8,14 @@ public class Window{
 	private int contains;//0 is an empty window, 1 is a criminal, 2 is an innocent
 	private int maxTime;
 	private int curTime;
+	private Rectangle rect;
 
 	public Window(int x, int y, int width, int height, int maxTime){
 		this.x = x;
 		this.y = y;
 		this.width = width;
 		this.height = height;
+		rect = new Rectangle(x, y, width, height);
 		this.maxTime = maxTime;
 		contains = 0;
 		curTime = 0;
@@ -23,6 +25,11 @@ public class Window{
 	public void setXY(int x, int y){
 		this.x = x;
 		this.y = y;
+		rect = new Rectangle(x, y, width, height);
+	}
+
+	public boolean clashesWith(Rectangle r2){
+		return rect.intersects(r2);
 	}
 
 
